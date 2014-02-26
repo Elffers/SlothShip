49c4c35de18e50c5cfd7976847e34d1f003652c0
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-26 15:37:47 -0800

    change input to come in via params

--- app/controllers/ups_controller.rb
+++ app/controllers/ups_controller.rb
@@ -4,8 +4,7 @@ include ActiveMerchant::Shipping
 class UpsController < ApplicationController
 
   def estimate
-    order = estimate_params
-    @estimate = extract_info(order)
+    @estimate = extract_info(estimate_params)
     respond_to do |format|
       format.html { render :estimate }
       format.json { render json: @estimate }
@@ -21,31 +20,34 @@ class UpsController < ApplicationController
   def extract_info(order)
     info = {} 
     response = ups_client.find_rates(order[:origin], order[:destination], order[:packages])
-    info[:price] = response.rates.select {|rate| rate.service_name == "UPS Standard"}.first.price
-    info[:delivery_date] = response.rates.select {|rate| rate.service_name == "UPS Standard"}.first.delivery_date
+    rates = response.rates
+    info[:options] = rates.map {|rate| [rate.service_name, rate.price, rate.delivery_date] }
+    # info[:price] = rates.price
+    # info[:delivery_date] = rate.delivery_date
     info
   end
 
   def estimate_params
-    # params.require(:order).permit(:origin, :destination, :packages)
-    estimate_hash = {}
-    estimate_hash[:packages] = [
-                Package.new(  100,                        # 100 grams
-                              [93,10],                    # 93 cm long, 10 cm diameter
-                              :cylinder => true),         # cylinders have different volume calculations
+    params.require(:order).permit(:origin, :destination, :packages)
+    # estimate_hash = {}
+    # estimate_hash[:packages] = [
+    #             Package.new(  100,                        # 100 grams
+    #                           [93,10],                    # 93 cm long, 10 cm diameter
+    #                           :cylinder => true),         # cylinders have different volume calculations
 
-                Package.new(  (7.5 * 16),                 # 7.5 lbs, times 16 oz/lb.
-                              [15, 10, 4.5],              # 15x10x4.5 inches
-                              :units => :imperial)        # not grams, not centimetres
-                ]
-    estimate_hash[:origin] = Location.new(  :country => 'US',
-                            :state => 'CA',
-                            :city => 'Beverly Hills',
-                            :zip => '90210')
-    estimate_hash[:destination] = Location.new( :country => 'CA',
-                            :province => 'ON',
-                            :city => 'Ottawa',
-                            :postal_code => 'K1P 1J1')
-    estimate_hash
+    #             Package.new(  (7.5 * 16),                 # 7.5 lbs, times 16 oz/lb.
+    #                           [15, 10, 4.5],              # 15x10x4.5 inches
+    #                           :units => :imperial)        # not grams, not centimetres
+    #             ]
+    # estimate_hash[:origin] = Location.new(  :country => 'US',
+    #                         :state => 'CA',
+    #                         :city => 'Beverly Hills',
+    #                         :zip => '90210')
+    # estimate_hash[:destination] = Location.new( :country => 'US',
+    #                         :state => 'WA',
+    #                         :city => 'Seattle',
+    #                         :postal_code => '98117')
+    # estimate_hash
+    # params
   end
 end
--- spec/controllers/ups_controller_spec.rb
+++ spec/controllers/ups_controller_spec.rb
@@ -12,6 +12,7 @@ describe UpsController do
                         :postal_code => 'K1P 1J1'
                       }}
     let(:package){ [100, [93,10], :cylinder => true] }
+    let(:params){ {origin: origin, destination: destination, packages: package} }
     let(:ups_client) { double("ups_client")}
     let(:extracted_info) { {"price" => 13437, "delivery_date" => "2014-02-27T00:00:00+00:00"} }
 
@@ -22,12 +23,12 @@ describe UpsController do
     end
 
     it 'should return JSON' do
-      get :estimate, format: :json
+      get :estimate, order: params, format: :json
       expect{JSON.parse(response.body)}.to_not raise_error
     end
 
     it 'should return estimated shipping price and delivery info' do
-      get :estimate, format: :json
+      get :estimate, order: params, format: :json
       order_info = JSON.parse(response.body)
       expect(order_info).to eq extracted_info
     end
