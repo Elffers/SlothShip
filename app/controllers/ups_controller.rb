357e5bf52822a3785787e7b99ad6097df3ad8c68
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-26 14:12:29 -0800

    passing spec for estimate returning json

--- app/controllers/ups_controller.rb
+++ app/controllers/ups_controller.rb
@@ -4,8 +4,41 @@ include ActiveMerchant::Shipping
 class UpsController < ApplicationController
 
   def estimate
-    origin = 
-    packages = [
+    order = estimate_params
+    # packages = [
+    #             Package.new(  100,                        # 100 grams
+    #                           [93,10],                    # 93 cm long, 10 cm diameter
+    #                           :cylinder => true),         # cylinders have different volume calculations
+
+    #             Package.new(  (7.5 * 16),                 # 7.5 lbs, times 16 oz/lb.
+    #                           [15, 10, 4.5],              # 15x10x4.5 inches
+    #                           :units => :imperial)        # not grams, not centimetres
+    #             ]
+    # origin = Location.new(  :country => 'US',
+    #                         :state => 'CA',
+    #                         :city => 'Beverly Hills',
+    #                         :zip => '90210')
+    # destination = Location.new( :country => 'CA',
+    #                         :province => 'ON',
+    #                         :city => 'Ottawa',
+    #                         :postal_code => 'K1P 1J1')
+    @estimate = ups_client.find_rates(order[:origin], order[:destination], order[:packages])
+    respond_to do |format|
+      format.html { render :estimate }
+      format.json { render json: @estimate }
+    end
+  end
+
+  private
+
+  def ups_client
+    UPS.new(:login => ENV['UPS_USERNAME'], :password => ENV['UPS_PASSWORD'], :key => ENV['UPS_ACCESS_KEY'])
+  end
+
+  def estimate_params
+    # params.require(:order).permit(:origin, :destination, :packages)
+    estimate_hash = {}
+    estimate_hash[:packages] = [
                 Package.new(  100,                        # 100 grams
                               [93,10],                    # 93 cm long, 10 cm diameter
                               :cylinder => true),         # cylinders have different volume calculations
@@ -14,23 +47,14 @@ class UpsController < ApplicationController
                               [15, 10, 4.5],              # 15x10x4.5 inches
                               :units => :imperial)        # not grams, not centimetres
                 ]
-    origin = Location.new(  :country => 'US',
+    estimate_hash[:origin] = Location.new(  :country => 'US',
                             :state => 'CA',
                             :city => 'Beverly Hills',
                             :zip => '90210')
-    destination = Location.new( :country => 'CA',
+    estimate_hash[:destination] = Location.new( :country => 'CA',
                             :province => 'ON',
                             :city => 'Ottawa',
                             :postal_code => 'K1P 1J1')
-
-    @estimate = ups_client.find_rates(origin, destination, packages)
-    respond_to do |format|
-        format.html { render :estimate }
-        format.json { render json: @estimate }
-      end
-  end
-
-  def ups_client
-    UPS.new(:login => ENV['UPS_USERNAME'], :password => ENV['UPS_PASSWORD'], :key => ENV['UPS_ACCESS_KEY'])
+    estimate_hash
   end
 end
--- spec/controllers/ups_controller_spec.rb
+++ spec/controllers/ups_controller_spec.rb
@@ -2,6 +2,28 @@ require 'spec_helper'
 
 describe UpsController do
   describe 'GET estimate' do
+    let(:origin){ { :country => 'US',
+                    :state => 'CA',
+                    :city => 'Beverly Hills',
+                    :zip => '90210'}}
+    let(:destination){{ :country => 'CA',
+                        :province => 'ON',
+                        :city => 'Ottawa',
+                        :postal_code => 'K1P 1J1'
+                      }}
+    let(:package){ [100, [93,10], :cylinder => true] }
+    let(:ups_client) { double("ups_client")}
+
+    before do
+      allow(controller).to receive(:ups_client).and_return ups_client
+      allow(ups_client).to receive(:find_rates).and_return({ origin: "hi"})
+    end
+
+    it 'should return JSON' do
+      get :estimate, format: :json
+      expect{JSON.parse(response.body)}.to_not raise_error
+    end
+    
   end
 
 end
