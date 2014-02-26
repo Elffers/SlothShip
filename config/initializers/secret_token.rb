30fd095ad81807001b3c719ffc020d80002dd477
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-26 10:39:48 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SlothShip::Application.config.secret_key_base = '64b366c8e850cd817ae84143d75299b8a056a45906ff9fbeffc6f30cf78b0f093841dfa243bad20ed5d46662496cb0655f1689b8f0714099efb6724a4b45f0b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SlothShip::Application.config.session_store :cookie_store, key: '_sloth_ship_session'
