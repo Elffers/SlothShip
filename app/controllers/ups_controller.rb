f7bfaa3f40a1087b80965597e2b5fda9f142e715
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-27 10:36:12 -0800

    refactoring broken, attempt to add params hash

--- app/controllers/ups_controller.rb
+++ app/controllers/ups_controller.rb
@@ -3,6 +3,23 @@ include ActiveMerchant::Shipping
 
 class UpsController < ApplicationController
 
+  # def send
+  #   # params = {:order => {:origin => [ :country => 'US',
+  #   #                                   :state => 'CA',
+  #   #                                   :city => 'Beverly Hills',
+  #   #                                   :zip => '90210'
+  #   #                                 ],
+  #   #                     :destination => [:country => 'US',
+  #   #                                     :state => 'WA',
+  #   #                                     :city => 'Seattle',
+  #   #                                     :postal_code => '98117'
+  #   #                                     ],
+  #   #                     :packages => [[100,[93,10],:cylinder => true]]
+  #   #                   }
+  #   #           }
+  #   # redirect_to estimate_path(params)
+  # end
+
   def estimate
     @estimate = extract_info(estimate_params)
     respond_to do |format|
@@ -22,32 +39,37 @@ class UpsController < ApplicationController
     response = ups_client.find_rates(order[:origin], order[:destination], order[:packages])
     rates = response.rates
     info[:options] = rates.map {|rate| [rate.service_name, rate.price, rate.delivery_date] }
-    # info[:price] = rates.price
-    # info[:delivery_date] = rate.delivery_date
+    # info[:price] = rates.first.price
+    # info[:delivery_date] = rate.first.delivery_date
     info
   end
 
   def estimate_params
-    params.require(:order).permit(:origin, :destination, :packages)
-    # estimate_hash = {}
-    # estimate_hash[:packages] = [
-    #             Package.new(  100,                        # 100 grams
-    #                           [93,10],                    # 93 cm long, 10 cm diameter
-    #                           :cylinder => true),         # cylinders have different volume calculations
+    # params.require(:order).permit(:origin, :destination, :packages)
+    estimate_hash = {}
+    # estimate_hash[:packages] = params[:packages].map {|package| Package.new(package)}
+    # estimate_hash[:origin] = Location.new(params[:origin])
+    # estimate_hash[:destination] = Location.new(params[:destination])
 
-    #             Package.new(  (7.5 * 16),                 # 7.5 lbs, times 16 oz/lb.
-    #                           [15, 10, 4.5],              # 15x10x4.5 inches
-    #                           :units => :imperial)        # not grams, not centimetres
-    #             ]
-    # estimate_hash[:origin] = Location.new(  :country => 'US',
-    #                         :state => 'CA',
-    #                         :city => 'Beverly Hills',
-    #                         :zip => '90210')
-    # estimate_hash[:destination] = Location.new( :country => 'US',
-    #                         :state => 'WA',
-    #                         :city => 'Seattle',
-    #                         :postal_code => '98117')
-    # estimate_hash
-    # params
+    estimate_hash[:packages]    = [Package.new( 100,                        # 100 grams
+                                                [93,10],                    # 93 cm long, 10 cm diameter
+                                                :cylinder => true),         # cylinders have different volume calculations
+
+                                  Package.new(  (7.5 * 16),                 # 7.5 lbs, times 16 oz/lb.
+                                                [15, 10, 4.5],              # 15x10x4.5 inches
+                                                :units => :imperial)        # not grams, not centimetres
+                                  ]
+    estimate_hash[:origin]      = Location.new( :country => 'US',
+                                            :state => 'CA',
+                                            :city => 'Beverly Hills',
+                                            :zip => '90210')
+    estimate_hash[:destination] = Location.new( :country => 'US',
+                                                :state => 'WA',
+                                                :city => 'Seattle',
+                                                :postal_code => '98117')
+
+    
+    estimate_hash
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SlothShip::Application.routes.draw do
 
-  get '/ups_estimate' => 'ups#estimate'
-  
+  get '/ups_estimate' => 'ups#estimate', as: :estimate
+  # get '/send' => "ups#send"
 end
