c35dfffafeb941c685877e6eb63e87a9a4d06263
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-27 14:24:02 -0800

    works correctly with simulated params hash passed in

--- app/controllers/ups_controller.rb
+++ app/controllers/ups_controller.rb
@@ -3,41 +3,44 @@ include ActiveMerchant::Shipping
 
 class UpsController < ApplicationController
 
+  #test method to simulate external request for estimate
   def go_estimate
-  
-    estimate_hash = {orders: {} }
-    estimate_hash[:orders][:packages] = [
-                              { weight:  100,                        
-                                dimensions: [93,10, 5],                    
-                                units: "metic" 
-                              },
+    estimate_hash = {order: {} }
+    estimate_hash[:order][:packages] =[ { weight: 100,                        
+                                          dimensions: [93, 10, 5],                    
+                                          units: "metric" 
+                                        },
 
-                              { weight: 7.5 * 16,
-                                dimensions: [15, 10, 4.5],              
-                                units: "imperial"
+                                        { weight: (7.5 * 16),
+                                          dimensions: [15, 10, 4.5],              
+                                          units: "imperial"
+                                        }
+                                      ]
+    estimate_hash[:order][:origin] =  { :country => 'US',
+                                :state => 'CA',
+                                :city => 'Beverly Hills',
+                                :zip => '90210'
                               }
-                            ]
-              estimate_hash[:orders][:origin] = {
-                                        :country => 'US',
-                                        :state => 'CA',
-                                        :city => 'Beverly Hills',
-                                        :zip => '90210'},
-              estimate_hash[:orders][:destination] = {
-                                              :country => 'US',
-                                              :state => 'WA',
-                                              :city => 'Seattle',
-                                              :postal_code => '98117'
-                                            }
-    
+
+    estimate_hash[:order][:destination] = { :country => 'US',
+                                    :state => 'WA',
+                                    :city => 'Seattle',
+                                    :postal_code => '98117'
+                                  }
     redirect_to generate_url("/ups_estimate.json", estimate_hash) 
 
   end
 
+  # private test helper method for above
   def generate_url(url, params = {})
+    #params hash is working correctly here
+    raise
     uri = URI(url)
     uri.query = params.to_query
     uri.to_s
   end
+
+  ###
 
   def estimate
     @estimate = extract_info(estimate_params)
@@ -67,32 +70,53 @@ class UpsController < ApplicationController
   end
 
   def estimate_params
-    #params comes back in from query string as hash, don't need to use Rack::Utils.parse_nested_query(query_string) after all
-    params.require(:order).permit(:destination, :packages)
+    # params comes back in from query string as hash (no need for explicit conversion), but the Rack::Utils.parse_nested_query(query_string) is not converting the hash correctly
+    # params.require(:order).permit(:destination, :packages)
     # expect :destination to be a hash containing sanitized address
     # expect :packages to be an array of hashes, each containing :weight, :dimensions, and :units (see https://github.com/Shopify/active_shipping/blob/master/lib/active_shipping/shipping/package.rb for other options)
 
     ### hard coded test option
-    estimate_hash = {}
-    estimate_hash[:packages] = [
-                Package.new(  100,                        # 100 grams
-                              [93,10],                    # 93 cm long, 10 cm diameter
-                              :cylinder => true),         # cylinders have different volume calculations
+    params_hash = {order: {} }
+    params_hash[:order][:packages]    = [ { weight: 100,                        
+                                            dimensions: [93, 10, 5],
+                                            :units => :metric 
+                                          },
 
-                Package.new(  (7.5 * 16),                 # 7.5 lbs, times 16 oz/lb.
-                              [15, 10, 4.5],              # 15x10x4.5 inches
-                              :units => :imperial)        # not grams, not centimetres
-                ]
-    estimate_hash[:origin] = Location.new(  :country => 'US',
-                            :state => 'CA',
-                            :city => 'Beverly Hills',
-                            :zip => '90210')
-    estimate_hash[:destination] = Location.new( :country => 'US',
-                            :state => 'WA',
-                            :city => 'Seattle',
-                            :postal_code => '98117')
-    estimate_hash
+                                          { weight: (7.5 * 16),
+                                            dimensions: [15, 10, 4.5],
+                                            :units => :imperial
+                                          }
+                                        ]
+    params_hash[:order][:origin]      = { :country => 'US',
+                                            :state => 'CA',
+                                            :city => 'Beverly Hills',
+                                            :zip => '90210'
+                                          }
 
-    ### end test option
+    params_hash[:order][:destination] = { :country => 'US',
+                                            :state => 'WA',
+                                            :city => 'Seattle',
+                                            :postal_code => '98117'
+                                          }
+
+    order = { origin: set_destination(params_hash[:order][:origin]), 
+              destination: set_origin(params_hash[:order][:destination]),
+              packages: set_packages(params_hash[:order][:packages])
+            }
+    order
+  end
+
+  def set_destination(destination_hash)
+    Location.new(destination_hash)
+  end
+
+  def set_origin(origin_hash)
+    Location.new(origin_hash)
+  end
+
+  def set_packages(packages)
+    packages.map do |package|
+      Package.new(package[:weight], package[:dimensions], units: package[:units])
+    end
   end
 end
