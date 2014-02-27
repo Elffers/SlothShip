389ad0c5d37bf23cc8c9a73d559e7f461da7e1a9
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-02-27 10:35:16 -0800

    working version

--- app/controllers/ups_controller.rb
+++ app/controllers/ups_controller.rb
@@ -28,26 +28,26 @@ class UpsController < ApplicationController
   end
 
   def estimate_params
-    params.require(:order).permit(:origin, :destination, :packages)
-    # estimate_hash = {}
-    # estimate_hash[:packages] = [
-    #             Package.new(  100,                        # 100 grams
-    #                           [93,10],                    # 93 cm long, 10 cm diameter
-    #                           :cylinder => true),         # cylinders have different volume calculations
+    # params.require(:order).permit(:origin, :destination, :packages)
+    estimate_hash = {}
+    estimate_hash[:packages] = [
+                Package.new(  100,                        # 100 grams
+                              [93,10],                    # 93 cm long, 10 cm diameter
+                              :cylinder => true),         # cylinders have different volume calculations
 
-    #             Package.new(  (7.5 * 16),                 # 7.5 lbs, times 16 oz/lb.
-    #                           [15, 10, 4.5],              # 15x10x4.5 inches
-    #                           :units => :imperial)        # not grams, not centimetres
-    #             ]
-    # estimate_hash[:origin] = Location.new(  :country => 'US',
-    #                         :state => 'CA',
-    #                         :city => 'Beverly Hills',
-    #                         :zip => '90210')
-    # estimate_hash[:destination] = Location.new( :country => 'US',
-    #                         :state => 'WA',
-    #                         :city => 'Seattle',
-    #                         :postal_code => '98117')
-    # estimate_hash
+                Package.new(  (7.5 * 16),                 # 7.5 lbs, times 16 oz/lb.
+                              [15, 10, 4.5],              # 15x10x4.5 inches
+                              :units => :imperial)        # not grams, not centimetres
+                ]
+    estimate_hash[:origin] = Location.new(  :country => 'US',
+                            :state => 'CA',
+                            :city => 'Beverly Hills',
+                            :zip => '90210')
+    estimate_hash[:destination] = Location.new( :country => 'US',
+                            :state => 'WA',
+                            :city => 'Seattle',
+                            :postal_code => '98117')
+    estimate_hash
     # params
   end
 end
