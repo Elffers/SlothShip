9857260adb8b16ae05ad20eb4766ba34cbb372c7
Sue White <sue@mailworks.org>
2014-02-28 12:39:00 -0800

    abstracts methods to application controller

--- app/controllers/fedex_controller.rb
+++ app/controllers/fedex_controller.rb
@@ -11,7 +11,6 @@ class FedexController < ApplicationController
     end
   end
 
-
   private
 
   def fedex_client
@@ -27,27 +26,4 @@ class FedexController < ApplicationController
     @fedex_rates = @estimate.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
   end
 
-  #temporary method
-  def estimate_params
-    # params.require(:order).permit(:origin, :destination, :packages)
-    estimate_hash = {}
-    estimate_hash[:packages] = [
-        Package.new(  100,                        # 100 grams
-                      [93,10],                    # 93 cm long, 10 cm diameter
-                      :cylinder => true),         # cylinders have different volume calculations
-
-        Package.new(  (7.5 * 16),                 # 7.5 lbs, times 16 oz/lb.
-                      [15, 10, 4.5],              # 15x10x4.5 inches
-                      :units => :imperial)        # not grams, not centimetres
-    ]
-    estimate_hash[:origin] = Location.new(  :country => 'US',
-                                            :state => 'CA',
-                                            :city => 'Beverly Hills',
-                                            :zip => '90210')
-    estimate_hash[:destination] = Location.new( :country => 'US',
-                                                :state => 'WA',
-                                                :city => 'Seattle',
-                                                :postal_code => '98117')
-    estimate_hash
-  end
 end
