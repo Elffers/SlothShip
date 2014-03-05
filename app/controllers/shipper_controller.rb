c4b1885e5a8e070f9b046b949d16517b2a1c4063
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-05 09:36:54 -0800

    updated routes

--- app/controllers/shipper_controller.rb
+++ app/controllers/shipper_controller.rb
@@ -27,36 +27,31 @@ class ShipperController < ApplicationController
   end
 
   #test method to simulate external request for estimate
-  def mock_external_request
-    estimate_hash = {order: {} }
-    estimate_hash[:order][:packages] =[ { weight: 100,
-                                          dimensions: "93, 10, 5", #breaks if this is an array
-                                          units: "metric" 
-                                        },
+  # def mock_external_request
+  #   estimate_hash = {order: {} }
+  #   estimate_hash[:order][:packages] =[ { weight: 100,
+  #                                         dimensions: "93, 10, 5", #breaks if this is an array
+  #                                         units: "metric" 
+  #                                       },
 
-                                        { weight: (7.5 * 16),
-                                          dimensions: "15, 10, 4.5",
-                                          units: "imperial"
-                                        }
-                                      ]
-    estimate_hash[:order][:origin] =  { :country => 'US',
-                                        :state => 'CA',
-                                        :city => 'Beverly Hills',
-                                        :zip => '90210'
-                                      }
+  #                                       { weight: (7.5 * 16),
+  #                                         dimensions: "15, 10, 4.5",
+  #                                         units: "imperial"
+  #                                       }
+  #                                     ]
+  #   estimate_hash[:order][:origin] =  { :country => 'US',
+  #                                       :state => 'CA',
+  #                                       :city => 'Beverly Hills',
+  #                                       :zip => '90210'
+  #                                     }
 
-    estimate_hash[:order][:destination] = { :country => 'US',
-                                            :state => 'WA',
-                                            :city => 'Seattle',
-                                            :postal_code => '98117'
-                                          }
+  #   estimate_hash[:order][:destination] = { :country => 'US',
+  #                                           :state => 'WA',
+  #                                           :city => 'Seattle',
+  #                                           :postal_code => '98117'
+  #                                         }
                                           
-    redirect_to "/get_estimate.json?#{estimate_hash.to_query}"
-  end
-
-  def hello
-    name = JSON.parse(params[:data])["name"]
-    render text: "hello, #{name}"
-  end
+  #   redirect_to "/get_estimate.json?#{estimate_hash.to_query}"
+  # end
 
 end
