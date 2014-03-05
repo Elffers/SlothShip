26f10d0ff8f938b8bf285cae666c0b289c637f3a
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-05 12:31:48 -0800

    specs on shipper for setting clients

--- spec/lib/shipper_spec.rb
+++ spec/lib/shipper_spec.rb
@@ -3,8 +3,22 @@ require 'spec_helper'
 describe Shipper do 
   #check webmock docs for how to store an actual call and tweak to get desired erros
 
-  it "handles bad zip code error" do
-    expect{ raise ArgumentError, "no"}.to raise_error ArgumentError
+  # xit "handles bad zip code error" do
+  #   expect{ raise ArgumentError, "no"}.to raise_error ArgumentError
+  # end
+
+  describe '.ups_client' do
+    it 'returns ups client' do
+      # client = double("ups client") #is this kind of silly?
+      # expect(UPS).to receive(:new){ client }
+      expect(Shipper.ups_client).to be_an_instance_of ActiveMerchant::Shipping::UPS
+    end
+  end
+
+  describe '.fedex_client' do
+    it 'returns fedex client' do
+      expect(Shipper.fedex_client).to be_an_instance_of ActiveMerchant::Shipping::FedEx
+    end
   end
 
 end
