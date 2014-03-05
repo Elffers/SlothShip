5d54ba9dcf7a54a93d1c0ab59c2de0865f7257ae
shanghaiellen@gmail.com <shanghaiellen@gmail.com>
2014-03-05 10:00:30 -0800

    fixes routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ SlothShip::Application.routes.draw do
 
   get '/' => 'shipper#mock_external_request'
   
-  get '/shipping_estimate' => 'shipper#estimate'
-  get '/get_fastest' => 'shipper#fastest'
-  get '/get_cheapest' => 'shipper#cheapest'
+  post '/shipping_estimate' => 'shipper#estimate'
+  post '/get_fastest' => 'shipper#fastest'
+  post '/get_cheapest' => 'shipper#cheapest'
 
   post '/hello' => 'shipper#hello'
   
