d097a70de9894eb10b0a722ea6b0e8b74eec0c8f
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-03-08 20:44:08 -0800

    change set to parse to appease rubocop

--- lib/order.rb
+++ lib/order.rb
@@ -2,20 +2,20 @@ class Order
   attr_reader :origin, :destination, :packages
 
   def initialize(params)
-    @origin       = origin(params[:order][:origin])
-    @destination  = destination(params[:order][:destination])
-    @packages     = packages(params[:order][:packages])
+    @origin       = parse_origin(params[:order][:origin])
+    @destination  = parse_destination(params[:order][:destination])
+    @packages     = parse_packages(params[:order][:packages])
   end
 
-  def destination(destination_hash)
+  def parse_destination(destination_hash)
     Location.new(destination_hash)
   end
 
-  def origin(origin_hash)
+  def parse_origin(origin_hash)
     Location.new(origin_hash)
   end
 
-  def packages(packages)
+  def parse_packages(packages)
     packages.map do |package|
       pkg_weight = package[:weight].to_i
       pkg_dimensions = package[:dimensions].split(',').map { |dimension| dimension.to_i }
